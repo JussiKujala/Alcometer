054b6dbcef3ff45c7852646945fe70f755ff45ff
Jussi Kujala <jussijustus@gmail.com>
2022-02-03 20:26:44 +0200

    code added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
+import { useState } from 'react';
 import './App.css';
-
-function handleSubmit(e){
-  e.preventDeafult();
-  let weight = 0;
-  if(gender == male){
-    
-  }
-}
 
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [drinks, setDrinks] = useState(0);
+  const [gender, setGender] = useState("male");
+  const [result, setResult] = useState(0);
+
+  function calculate(e) {
+    e.preventDefault();
+    let alcoholburn = 3;
+    if (gender === 'male') {
+      alcoholburn = 1;
+    }
+    else {
+      alcoholburn = 2;
+    }
+    setResult(alcoholburn);
+  }
+
+
+
   return (
-    <form>
+    <form onSubmit={calculate}>
       <div>
         <label>Body Weight (kg)</label>
-        <input type="number" onChange={e => setWeight(e.target.value)} />
+        <input type="number" value={weight} onChange={e => setWeight(e.target.value)} />
       </div>
       <div>
-        <label>Amount of Alcohol Consumed(33cl bottles)</label>
-        <input type="text" />
+        <label>Amount of Alcohol Consumed (33cl bottles):</label>
+        <input type="number" value={drinks} onChange={e => setDrinks(e.target.value)} />
       </div>
       <div>
         <label>Gender:</label>
-        <label> <input type="radio" name="gender" defaultChecked onChange={e => setGender(e.target.value)} />Male</label>
-        <label> <input type="radio" name="gender" onChange={e => setGender(e.target.value)} />Female</label>
+        <label> <input type="radio" value="male" defaultChecked onChange={e => setGender(e.target.value)} />Male</label>
+        <label> <input type="radio" value="female" onChange={e => setGender(e.target.value)} />Female</label>
       </div>
       <div>
-        <label>Time Since First Drink:</label> <input type="text" />
+        <label>Time Since First Drink (hours):</label> <input type="text" />
       </div>
-      <input type="submit" />
+      <div>
+        <label>Your Blood Alcohol Concentration</label>
+        <output>{result.toFixed(0)}</output>
+      </div>
+      <button>Submit</button>
     </form>
   );
 }
-
-
 
 export default App;
 
